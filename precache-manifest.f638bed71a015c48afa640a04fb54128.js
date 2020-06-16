3a846cfdbbdfa9b011376be3f2f4986677856eed
Евгений <evgenij.romanko@neoseo.com.ua>
2020-06-16 22:58:02 +0300

    Updates

---
+++ precache-manifest.f638bed71a015c48afa640a04fb54128.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34258797891041c9bb7aae7658da3816",
+    "url": "/18-react-hw-01-component/index.html"
+  },
+  {
+    "revision": "8817a773470f9af3b346",
+    "url": "/18-react-hw-01-component/static/css/main.dd1e033c.chunk.css"
+  },
+  {
+    "revision": "2fe9267e49139c1fb03e",
+    "url": "/18-react-hw-01-component/static/js/2.9191068f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/18-react-hw-01-component/static/js/2.9191068f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8817a773470f9af3b346",
+    "url": "/18-react-hw-01-component/static/js/main.fca61944.chunk.js"
+  },
+  {
+    "revision": "f01b746042b25d719079",
+    "url": "/18-react-hw-01-component/static/js/runtime-main.085fba54.js"
+  }
+]);
